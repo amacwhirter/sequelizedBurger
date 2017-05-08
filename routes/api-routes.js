f9121929668ce5c21e572a7e9611864d1d1324f0
Ashley MacWhirter <macwhirtera@gmail.com>
2017-05-08 00:10:04 -0400

    WIP

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,16 +12,21 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-  app.get("/api/burgers", function(req, res) {
-    // Add sequelize code to find all posts, and return them to the user with res.json
+  // GET route for getting all of the burgers
+  app.get("/burgers", function(req, res) {
+    // findAll returns all entries for a table when used with no options
     db.Burger.findAll({}).then(function(dbBurger) {
-      res.json(dbBurger);
+      // We have access to the todos as an argument inside of the callback function
+      var hbsObject = {
+        dbBurger: dbBurger
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
     });
   });
 
   // POST route for saving a new post
-  app.post("/api/burgers", function(req, res) {
+  app.post("/", function(req, res) {
     // Add sequelize code for creating a post using req.body,
     // then return the result using res.json
     db.Burger.create({
@@ -33,7 +38,7 @@ module.exports = function(app) {
   });
 
   // PUT route for updating posts
-  app.put("/api/burgers", function(req, res) {
+  app.put("/api/:id", function(req, res) {
     // Add code here to update a post using the values in req.body, where the id is equal to
     // req.body.id and return the result to the user using res.json
     db.Burger.update({
